5118b99c77decdcc78a6968f868efb2bf7a4d4b0
Madusanka Madiligama <83027416+mbabeykoon@users.noreply.github.com>
2024-03-07 03:24:35 -0600

    prepare callbacks added

--- main.py
+++ main.py
@@ -16,13 +16,15 @@ from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelT
 #         logger.exception(e)
 #         raise e
 
-STAGE_NAME = "Prepare base model"
-try: 
-   logger.info(f"*******************")
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   prepare_base_model = PrepareBaseModelTrainingPipeline()
-   prepare_base_model.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-        logger.exception(e)
-        raise e
+# STAGE_NAME = "Prepare base model"
+# try: 
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    prepare_base_model = PrepareBaseModelTrainingPipeline()
+#    prepare_base_model.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+
